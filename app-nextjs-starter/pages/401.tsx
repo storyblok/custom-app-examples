561fdf3388e7ecee8773f5ec72f73ff00e6b75a7
Eunjae Lee <hey@eunjae.dev>
2023-03-28 14:06:12 +0200

    fix: redirect properly after auth flow (#2)

--- app-nextjs-starter/pages/401.tsx
+++ app-nextjs-starter/pages/401.tsx
@@ -1,11 +1,20 @@
 import {NextPage} from "next";
+import {useEffect} from "react";
 
 import {Layout} from '@src/components/Layout'
 
-const Page: NextPage = () => (
-  <Layout>
-    Failed to authenticate
-  </Layout>
-)
+const Page: NextPage = () => {
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.top === window.self) {
+      window.location.assign("https://app.storyblok.com/oauth/app_redirect");
+    }
+  }, []);
+
+  return (
+    <Layout>
+      Failed to authenticate
+    </Layout>
+  );
+}
 
 export default Page
