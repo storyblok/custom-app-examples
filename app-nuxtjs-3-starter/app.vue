078e7ed669bd5ffd17aaf7e92d9caf262b45aea3
Johannes Lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2023-05-23 21:20:21 +0200

    feat: starter with Nuxt 3

---
+++ app-nuxtjs-3-starter/app.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import {navigateTo, useFetch, useRoute} from '#app'
+import { initOauthFlowUrl } from '~/src/endpointPrefix'
+
+const route = useRoute()
+const { spaceId, userId } = route.query
+
+if (!spaceId || !userId) {
+  navigateTo(initOauthFlowUrl)
+}
+const result = await useFetch(() => `/api/stories`, {
+  params: {
+    spaceId,
+    userId,
+  },
+})
+</script>
+<template>
+  <div v-if="!result.pending.value">
+    <p>
+      Hello, these are the first {{ result.data.length }} stories on this space:
+    </p>
+    <Stories :stories="result.data.value" />
+  </div>
+</template>
