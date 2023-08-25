74a7a15abe6176152dee1dc5f4f106a72ee65182
Demetrius Feijóo <demetrius.feijoo@storyblok.com>
2023-08-25 08:41:20 -0300

    remove isLastAuthor validation

--- app-nextjs-starter/src/Story.ts
+++ app-nextjs-starter/src/Story.ts
@@ -11,12 +11,6 @@ export type Story = {
   last_author: LastAuthor
 }
 
-const isLastAuthor = (data: unknown): data is LastAuthor =>
-  typeof data === 'object' &&
-  data !== null &&
-  'friendly_name' in data &&
-  typeof data.friendly_name === 'string'
-
 export const isStory = (data: unknown): data is Story =>
   typeof data === 'object' &&
   data !== null &&
@@ -30,8 +24,7 @@ export const isStory = (data: unknown): data is Story =>
   typeof data.slug === 'string' &&
   'updated_at' in data &&
   typeof data.updated_at === 'string' &&
-  'last_author' in data &&
-  isLastAuthor(data.last_author)
+  'last_author' in data
 
 export const isStories = (data: unknown): data is Story[] =>
   Array.isArray(data) && data.every(isStory)
